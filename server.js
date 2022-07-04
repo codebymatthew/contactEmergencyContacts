8a04d409f4c91604052fd7347782a69afe199001
Matthew Mitchell <matthew.mitchell.job@gmail.com>
2022-07-04 11:14:57 -0600

    added mongoDB basic functionality

--- server.js
+++ server.js
@@ -1,13 +1,55 @@
+// MIDDLEWARE / MODULES
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+// PARAMETERS
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
-
+const mongoPW = process.env.MONGODB_PW
+const PORT = 8000
+const connectionString = `mongodb+srv://focodevsproject001:${mongoPW}@cluster0.rrtbx.mongodb.net/?retryWrites=true&w=majority`
 const twilio = require('twilio')
 const client = new twilio(accountSid, authToken)
 
-client.messages
-        .create({
-            body: insideBody,
-            to: inputedNumber,
-            from: '+12525955686'
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('emergency-messaging-system')
+    const numbersCollection = db.collection('numbers')
+    app.post('/submit-form', (req, res) => {
+      console.log(req.body.smsMessage)
+      numbersCollection.insertOne(req.body)
+        .then(result => {
+          console.log(result)
         })
-        .then((message) => console.log(message.sid))
+        .catch(error => console.error(error))
+      })
+  })
+  .catch(error => console.error(error))
+    
+    app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html')
+})
+
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`The server is currently running on port: ${PORT}`)
+})
+
+
+
+// client.messages
+//         .create({
+//             body: req.body.smsMessage,
+//             to: req.body.phoneNumber,
+//             from: '+12525955686'
+//         })
+//         .then((message) => console.log(message.sid))
