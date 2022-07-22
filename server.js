61e7dff050485acdbc93b152b00d63410e5579d8
Matthew Mitchell <matthew.mitchell.job@gmail.com>
2022-07-22 16:27:51 -0600

    updated app to create pre made messages

--- server.js
+++ server.js
@@ -10,36 +10,58 @@ require('dotenv').config()
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const mongoPW = process.env.MONGODB_PW
+const mongoID = process.env.MONGODB_ID
 const PORT = 8000
 const connectionString = `mongodb+srv://focodevsproject001:${mongoPW}@cluster0.rrtbx.mongodb.net/?retryWrites=true&w=majority`
 const twilio = require('twilio')
 const client = new twilio(accountSid, authToken)
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('emergency-messaging-system')
-    const numbersCollection = db.collection('numbers')
+.then(client => {
+  console.log('Connected to Database')
+  const db = client.db('emergency-messaging-system')
+  const numbersCollection = db.collection('numbers')
+  const messagesCollection = db.collection('messages')
+  app.set('view engine', 'ejs')
+  app.use(express.static(__dirname + '/public'))
+  app.use(cors())
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(express.urlencoded({extended: true}))
 
-    
     app.post('/submit-form', (req, res) => {
-      console.log(req.body.smsMessage)
       numbersCollection.insertOne(req.body)
         .then(result => {
           console.log(result)
+          res.redirect('/')
         })
         .catch(error => console.error(error))
       })
+    app.post('/message-form', (req, res) => {
+      console.log(req.body.message)
+      messagesCollection.insertOne(req.body)
+      .then(result=> {
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+    })
+
+
+    app.get('/', async (req, res) => {
+      let contactVar = await numbersCollection.find().toArray()
+      let messagesVar = await messagesCollection.find().toArray()  
+      console.log(contactVar)
+      console.log(messagesVar)
+      res.render('index.ejs', {contacts: contactVar, messages: messagesVar})
+    })
+
+    app.post('/send-message', (req, res) => {
+      console.log(req.body)
+    })
+
   })
   .catch(error => console.error(error))
     
-    app.use(cors())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.urlencoded({extended: true}))
 
-app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html')
-})
 
 
 app.listen(process.env.PORT || PORT, () => {
